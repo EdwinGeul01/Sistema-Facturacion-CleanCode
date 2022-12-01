9719ac865086ec6d52e0a8c6e6307e4db424c23e
Edwin Geul <edwingeul01@gmail.com>
2022-12-01 13:17:33 -0600

    Add
    Added:
         Home btn
         Total display

--- src/components/NavUser.tsx
+++ src/components/NavUser.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogOutButton from './Buttons/LogOutButton';
+import HomeIcon from '@mui/icons-material/Home';
+
+import { useNavigate } from 'react-router-dom';
 
 export default function NavUser() {
+  
+  let RedirectToURL = useNavigate();
+  
   return (
     <div className='h-[8vh] bg-white w-[100]  flex justify-center items-center '>
+
 
       <div className='w-[80%] flex flex-row items-center'>
       <AccountCircleIcon
@@ -15,8 +22,25 @@ export default function NavUser() {
       className='text-red-400'/>
       <span
       className='text-slate-500 mx-5'>USUARIO</span>
+      
+      <div>
+
+      <button onClick={()=>{
+
+        RedirectToURL('/home');
+
+      }}>
+        <HomeIcon className='text-slate-300 hover:text-slate-500 duration-300'
+        sx={{
+          padding:"3px",
+          fontSize:40
+        }} />
+      </button>
+      
       </div>
       
+      </div>
+
       <LogOutButton />
 
 
